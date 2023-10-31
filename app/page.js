23185ae8c280053f5f72f8316b5b333c3b0b1986
Robert Nemeth <104597057+Robertsnemeth@users.noreply.github.com>
2023-10-31 13:42:49 -0700

    finished mobile

--- app/page.js
+++ app/page.js
@@ -1,9 +1,79 @@
-import Image from 'next/image'
+"use client"
+import Image from 'next/image';
+import { useState, useRef } from 'react';
+import List from '../public/images/icon-list.svg'
+import Success from '../public/images/icon-success.svg';
+import MobileSigunup from '../public/images/illustration-sign-up-mobile.svg';
+import DesktopSignup from '../public/images/illustration-sign-up-desktop.svg';
+const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-    </main>
+  const [ email, setEmail ] = useState('');
+  const [ success, setSuccess ] = useState(true);
+  const [ error, setError ] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!EMAIL_REGEX.test(email)) {
+      setError(true);
+      return;
+    } 
+    setSuccess(true);
+    setError(false);
+  }
+  
+  const handleEmail = (e) => {
+    console.log(email);
+    setEmail(e.target.value)
+  }
+
+  const handleDismiss = () => {
+    setSuccess(false);
+    setEmail('');
+  }
+
+  return (
+    <>
+    {success ? 
+      <main className='bg-white min-h-screen min-w-screen flex flex-col p-8 items-start justify-between'>
+        <section className="w-[375px] p-4 text-black font-bold flex flex-col gap-8 mt-36">
+          <Image src={Success} alt="header" width={100} height={100} />
+          <h1 className="text-4xl font-bold text-dark-slate-grey">Thanks for subscribing!</h1>
+          <p className='text-[16px]'>A confirmation email has been sent to {email}. Please open it and click the button inside to confirm your subscription.</p>
+        </section>
+        <button onClick={() => handleDismiss()} className='bg-neutral-dark-slate-grey text-white rounded w-full h-12 hover:bg-primary-tomato self-baseline'>Dismiss notification</button>
+      </main> 
+      :
+      <main className="flex min-h-screen min-w-screen flex-col items-start bg-white text-black">
+          <Image src={MobileSigunup} alt="header" className="w-full h-1/3" />
+          <section className='flex flex-col p-8 gap-10 items-start w-[375px]'>
+            <h1 className="text-4xl font-bold text-dark-slate-grey">Stay Updated!</h1>
+            <p className='text-[16px]'>Join 60,000+ products managers receiving monthly updates on.</p>
+            <section className='flex flex-col gap-2'>
+              <div className='flex gap-4 items-start'>
+                <Image src={List} alt="list" className="" />
+                <p className='text-[16px]'>Product discovery and building what matters</p>
+              </div> 
+              <div className='flex gap-4 items-start'>
+                <Image src={List} alt="list" className="" />
+                <p className='text-[16px]'>Measuring to ensure updates are a success</p>
+              </div> 
+              <div className='flex gap-4 items-start'>
+                <Image src={List} alt="list" className="" />
+                <p className='text-[16px]'>And much more!</p>
+              </div>  
+            </section>
+            <form className='w-full flex flex-col gap-4' onSubmit={handleSubmit}>
+              <div className='flex flex-col gap-2 w-full'>
+                <label htmlFor='email' className='text-sm font-bold'>Email Address</label>
+                {error && <p className='text-xs text-primary-tomato'>Please enter a valid email address</p>}
+                <input onChange={handleEmail} name="email" value={email} type="email" placeholder="email@company.com" className={`border border-neutral-charcaol-grey rounded w-full h-12 p-3`}/>
+              </div>
+              <button className='bg-neutral-dark-slate-grey text-white rounded w-full h-12 hover:bg-primary-tomato'>Subscribe to monthly newsletter</button>
+            </form>
+          </section>
+    </main>}
+        </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          'tomato': 'hsl(4, 100%, 67%)'
+        },
+        neutral: {
+          'dark-slate-grey': 'hsl(234, 29%, 20%)',
+          'charcoal-grey': 'hsl(235, 18%, 26%)',
+          'grey': 'hsl(231, 7%, 60%)',
+          'white': 'hsl(0, 0%, 100%)'
+        },
       },
     },
   },
